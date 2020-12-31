0b006819ce03297f40660a6d8db78912453f510b
Huzzai <2665556+Huzzai@users.noreply.github.com>
2020-12-31 05:49:16 -0800

    Update SmartCane.ino

--- SmartCane.ino
+++ SmartCane.ino
@@ -5,6 +5,10 @@
 #include <math.h>
 #include "notes.h"
 volatile long durationOneWay_1, durationOneWay_2, durationOneWay_3, duration_1, duration_2, duration_3;
+//========================================================//
+int melody[] = {NOTE_E7, NOTE_E7, 0, NOTE_E7, 0, NOTE_C7, NOTE_E7, 0, NOTE_G7, 0, 0, 0, NOTE_G6, 0, 0, 0};
+int noteDurations[] = {12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12, 12};
+//=============================================================//
 const int pin_trig_down = 6;                                     // Trig Pin Down sensor
 const int pin_echo_down = 3;                                     // Echo Pin Down sensor
 const int pin_trig_middle = 4;                                   // Trig Pin middle sensor
